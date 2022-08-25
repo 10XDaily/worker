ed139f0edee104842eedfd4a9912c282f16ea15f
Chris Jacob <i.chris.jacob@gmail.com>
2022-08-25 22:22:03 +1000

    Update index.js
    Adding BASIC AUTH from https://developers.cloudflare.com/workers/examples/basic-auth/
    Hardcoded as:
    const BASIC_USER = 'admin';
    const BASIC_PASS = 'pass';

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ import { Router } from 'itty-router'
 // Create a new router
 const router = Router()
 
+// Basic Auth protection of private pages https://developers.cloudflare.com/workers/examples/basic-auth/
+const BASIC_USER = 'admin';
+const BASIC_PASS = 'pass';
+
+router.get("/logout", () => {
+  console.log("logout logs");
+  // Invalidate the "Authorization" header by returning a HTTP 401.
+  // We do not send a "WWW-Authenticate" header, as this would trigger
+  // a popup in the browser, immediately asking for credentials again.
+  return new Response('Logged out.', { status: 401 });
+})
+
 /*
-Our index route, a simple hello world.
+Our index route, a simple hello world. No login required.
 */
 router.get("/", () => {
   console.log("index logs");
@@ -27,14 +39,29 @@ const X_API_LEXPAD = "lexpad" // https://x8ki-letl-twmt.n7.xano.io/apidoc:xhF9IG
 router.get("/newsletter", async request => {
   console.log("newsletter logs");
   
-  let today = new Date(); // Cloudflare workers freeze time, see https://developers.cloudflare.com/workers/learning/security-model/
-  let endpoint = `${GR_API}${GR_API_NEWSLETTERS}`;
-  let html_style = `body{padding:6em; font-family: sans-serif;} h1{color:#f6821f}`;
-  let html_content = '<h1>Success!!!</h1>';
+  const { protocol, pathname } = new URL(request.url);
+
+  // In the case of a Basic authentication, the exchange MUST happen over an HTTPS (TLS) connection to be secure.
+  if ('https:' !== protocol || 'https' !== request.headers.get('x-forwarded-proto')) {
+    throw new BadRequestException('Please use a HTTPS connection.');
+  }
   
-  let email_json = {
-    "content": {
-      "html": `
+  // The "Authorization" header is sent when authenticated.
+  if (request.headers.has('Authorization')) {
+    // Throws exception when authorization fails.
+    const { user, pass } = basicAuthentication(request);
+    verifyCredentials(user, pass);
+
+    // Only returns this response when no exception is thrown.
+  
+    let today = new Date(); // Cloudflare workers freeze time, see https://developers.cloudflare.com/workers/learning/security-model/
+    let endpoint = `${GR_API}${GR_API_NEWSLETTERS}`;
+    let html_style = `body{padding:6em; font-family: sans-serif;} h1{color:#f6821f}`;
+    let html_content = '<h1>Success!!!</h1>';
+
+    let email_json = {
+      "content": {
+        "html": `
 <p>
 “Knowledge is Power ⚡ Money is Freedom”
 <br>— 10X Daily
@@ -59,7 +86,7 @@ router.get("/newsletter", async request => {
 <br>Subscriber ID: {{CONTACT \`subscriber_id\`}}
 </p>
 `,
-      "plain": `
+        "plain": `
 “Knowledge is Power ⚡ Money is Freedom”
 — 10X Daily
 
@@ -79,81 +106,92 @@ Message ID: {{CONTACT \`message_id\`}}
 Subscriber ID: {{CONTACT \`subscriber_id\`}}
 
 `// TODO - dynamic plaintext version of the HTML email? (strip HTML)
-    },
-    "flags": [
-      "openrate",
-      "clicktrack"
-      // "google_analytics" // requires higher paid plan. Adds UTM tracking on links in email, to be tracked on our Website
-    ],
-    "name": today.toISOString() + ' 10X DAILY', // TODO make timezone aware (e.g. Australia/Sydney). Note that .toISOString() always returns a timestamp in UTC
-    "type": "broadcast", // draft or broadcast
-    "editor": "custom",
-    "subject": '10X [[firstname mode="uc"]] ⚡ {{DATE "DAY_ORDINATED MONTH_NAME YEAR"}}',
-    "fromField": {
-      "fromFieldId": "oqRaG" // "KO8SL" // 10X Daily <hello@10x.day> // curl -H "X-Auth-Token: api-key ____________" https://api.getresponse.com/v3/from-fields
-    },
-    "replyTo": {
-      "fromFieldId": "oqRaG" // "KO8SL" // 10X Daily <hello@10x.day> // curl -H "X-Auth-Token: api-key ____________" https://api.getresponse.com/v3/from-fields
-    },
-    "campaign": {
-      "campaignId": "LCJtj" // "Q1Oz0" // "10X Daily" subscriber list // curl -H "X-Auth-Token: api-key ____________" https://api.getresponse.com/v3/campaigns
-    },
-//    "sendOn": "2022-08-13T05:39:55+10:00", // omitted to send message immediately i.e. the manual trigger or 5am CRON trigger will send the message
-//     "attachments": [
-//       {
-//         "fileName": "some_file.jpg",
-//         "content": "sdfadsfetsdjfdskafdsaf==",
-//         "mimeType": "image/jpeg"
-//       }
-//     ], // No attachements needed. 400kb max combined size if needed in the future.
-    "sendSettings": {
-      "selectedCampaigns": ["LCJtj"], // ["Q1Oz0"], // "10X Daily" subscriber list
-      "selectedSegments": [], // TODO add Custom Field "UTC Offset Timezone" with 25 values "UTC -12"... "UTC 0" ... "UTC +12". Use for 5am email delivery.
-      "selectedSuppressions": [],
-      "excludedCampaigns": [],
-      "excludedSegments": [],
-      "selectedContacts": ["V5p8EtA"], // ["VohAb0F"], // Contact ID for email subscriber "test+5@10x.day" // {campaignId} = Q1Oz0 // curl -H "X-Auth-Token: api-key ____________" https://api.getresponse.com/v3/campaigns/Q1Oz0/contacts
-      "timeTravel": "false", // requires higher paid plan. Instead we will use a Segment, and user defined Custom Field "UTC Offset Timezone".
-      "perfectTiming": "false",
-      "externalLexpad": {
-         "dataSourceUrl": X_API + X_API_LEXPAD,
-         "dataSourceToken": X_API_KEY
+      },
+      "flags": [
+        "openrate",
+        "clicktrack"
+        // "google_analytics" // requires higher paid plan. Adds UTM tracking on links in email, to be tracked on our Website
+      ],
+      "name": today.toISOString() + ' 10X DAILY', // TODO make timezone aware (e.g. Australia/Sydney). Note that .toISOString() always returns a timestamp in UTC
+      "type": "broadcast", // draft or broadcast
+      "editor": "custom",
+      "subject": '10X [[firstname mode="uc"]] ⚡ {{DATE "DAY_ORDINATED MONTH_NAME YEAR"}}',
+      "fromField": {
+        "fromFieldId": "oqRaG" // "KO8SL" // 10X Daily <hello@10x.day> // curl -H "X-Auth-Token: api-key ____________" https://api.getresponse.com/v3/from-fields
+      },
+      "replyTo": {
+        "fromFieldId": "oqRaG" // "KO8SL" // 10X Daily <hello@10x.day> // curl -H "X-Auth-Token: api-key ____________" https://api.getresponse.com/v3/from-fields
+      },
+      "campaign": {
+        "campaignId": "LCJtj" // "Q1Oz0" // "10X Daily" subscriber list // curl -H "X-Auth-Token: api-key ____________" https://api.getresponse.com/v3/campaigns
+      },
+  //    "sendOn": "2022-08-13T05:39:55+10:00", // omitted to send message immediately i.e. the manual trigger or 5am CRON trigger will send the message
+  //     "attachments": [
+  //       {
+  //         "fileName": "some_file.jpg",
+  //         "content": "sdfadsfetsdjfdskafdsaf==",
+  //         "mimeType": "image/jpeg"
+  //       }
+  //     ], // No attachements needed. 400kb max combined size if needed in the future.
+      "sendSettings": {
+        "selectedCampaigns": ["LCJtj"], // ["Q1Oz0"], // "10X Daily" subscriber list
+        "selectedSegments": [], // TODO add Custom Field "UTC Offset Timezone" with 25 values "UTC -12"... "UTC 0" ... "UTC +12". Use for 5am email delivery.
+        "selectedSuppressions": [],
+        "excludedCampaigns": [],
+        "excludedSegments": [],
+        "selectedContacts": ["V5p8EtA"], // ["VohAb0F"], // Contact ID for email subscriber "test+5@10x.day" // {campaignId} = Q1Oz0 // curl -H "X-Auth-Token: api-key ____________" https://api.getresponse.com/v3/campaigns/Q1Oz0/contacts
+        "timeTravel": "false", // requires higher paid plan. Instead we will use a Segment, and user defined Custom Field "UTC Offset Timezone".
+        "perfectTiming": "false",
+        "externalLexpad": {
+           "dataSourceUrl": X_API + X_API_LEXPAD,
+           "dataSourceToken": X_API_KEY
+        }
       }
     }
+
+    const init = {
+      headers: {
+        'content-type': 'application/json;charset=UTF-8',
+        'X-Time-Zone': 'Australia/Sydney', // the default timezone in response data is UTC (if I remove this header)
+        'X-Auth-Token': 'api-key ' + GR_API_KEY
+      },
+      body: JSON.stringify(email_json),
+      method: 'POST'
+    };
+
+    const response = await fetch(endpoint, init);
+    const content = await response.json();
+
+    console.log(content);
+
+  //   html_content += `<p>... add more HTML to confirm the email sent successfully</p>`; // TODO
+
+    let html = `
+  <!DOCTYPE html>
+  <head>
+    <title>Newsletter: Send</title>
+  </head>
+  <body>
+    <style>${html_style}</style>
+    <div id="container">
+    ${html_content}
+    </div>
+  </body>`;
+
+    return new Response(html, {
+      headers: {
+        'content-type': 'text/html;charset=UTF-8',
+        'Cache-Control': 'no-store',
+      },
+    });
   }
   
-  const init = {
+  // Not authenticated.
+  return new Response('You need to login.', {
+    status: 401,
     headers: {
-      'content-type': 'application/json;charset=UTF-8',
-      'X-Time-Zone': 'Australia/Sydney', // the default timezone in response data is UTC (if I remove this header)
-      'X-Auth-Token': 'api-key ' + GR_API_KEY
-    },
-    body: JSON.stringify(email_json),
-    method: 'POST'
-  };
-
-  const response = await fetch(endpoint, init);
-  const content = await response.json();
-  
-  console.log(content);
-
-//   html_content += `<p>... add more HTML to confirm the email sent successfully</p>`; // TODO
-
-  let html = `
-<!DOCTYPE html>
-<head>
-  <title>Newsletter: Send</title>
-</head>
-<body>
-  <style>${html_style}</style>
-  <div id="container">
-  ${html_content}
-  </div>
-</body>`;
-
-  return new Response(html, {
-    headers: {
-      'content-type': 'text/html;charset=UTF-8',
+      // Prompts the user for credentials.
+      'WWW-Authenticate': 'Basic realm="my scope", charset="UTF-8"',
     },
   });
 })
@@ -222,6 +260,72 @@ router.get("/:slug", ({ params, query }) => {
 })
 
 
+/**
+ * Throws exception on verification failure.
+ * @param {string} user
+ * @param {string} pass
+ * @throws {UnauthorizedException}
+ */
+function verifyCredentials(user, pass) {
+  if (BASIC_USER !== user) {
+    throw new UnauthorizedException('Invalid credentials.');
+  }
+
+  if (BASIC_PASS !== pass) {
+    throw new UnauthorizedException('Invalid credentials.');
+  }
+}
+
+/**
+ * Parse HTTP Basic Authorization value.
+ * @param {Request} request
+ * @throws {BadRequestException}
+ * @returns {{ user: string, pass: string }}
+ */
+function basicAuthentication(request) {
+  const Authorization = request.headers.get('Authorization');
+
+  const [scheme, encoded] = Authorization.split(' ');
+
+  // The Authorization header must start with Basic, followed by a space.
+  if (!encoded || scheme !== 'Basic') {
+    throw new BadRequestException('Malformed authorization header.');
+  }
+
+  // Decodes the base64 value and performs unicode normalization.
+  // @see https://datatracker.ietf.org/doc/html/rfc7613#section-3.3.2 (and #section-4.2.2)
+  // @see https://dev.mozilla.org/docs/Web/JavaScript/Reference/Global_Objects/String/normalize
+  const buffer = Uint8Array.from(atob(encoded), character => character.charCodeAt(0));
+  const decoded = new TextDecoder().decode(buffer).normalize();
+
+  // The username & password are split by the first colon.
+  //=> example: "username:password"
+  const index = decoded.indexOf(':');
+
+  // The user & password are split by the first colon and MUST NOT contain control characters.
+  // @see https://tools.ietf.org/html/rfc5234#appendix-B.1 (=> "CTL = %x00-1F / %x7F")
+  if (index === -1 || /[\0-\x1F\x7F]/.test(decoded)) {
+    throw new BadRequestException('Invalid authorization value.');
+  }
+
+  return {
+    user: decoded.substring(0, index),
+    pass: decoded.substring(index + 1),
+  };
+}
+
+function UnauthorizedException(reason) {
+  this.status = 401;
+  this.statusText = 'Unauthorized';
+  this.reason = reason;
+}
+
+function BadRequestException(reason) {
+  this.status = 400;
+  this.statusText = 'Bad Request';
+  this.reason = reason;
+}
+
 /*
 This is the last route we define, it will match anything that hasn't hit a route we've defined
 above, therefore it's useful as a 404 (and avoids us hitting worker exceptions, so make sure to include it!).
@@ -235,5 +339,20 @@ This snippet ties our worker to the router we deifned above, all incoming reques
 are passed to the router where your routes are called and the response is sent.
 */
 addEventListener('fetch', (e) => {
-  e.respondWith(router.handle(e.request))
+  e.respondWith(router.handle(e.request).catch(err => {
+      const message = err.reason || err.stack || 'Unknown Error';
+
+      return new Response(message, {
+        status: err.status || 500,
+        statusText: err.statusText || null,
+        headers: {
+          'Content-Type': 'text/plain;charset=UTF-8',
+          // Disables caching by default.
+          'Cache-Control': 'no-store',
+          // Returns the "Content-Length" header for HTTP HEAD requests.
+          'Content-Length': message.length,
+        },
+      });
+    })
+  )
 })
