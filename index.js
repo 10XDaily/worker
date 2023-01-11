9c7a018ab22741b71d2429aa6068f99537bf8026
Chris Jacob <i.chris.jacob@gmail.com>
2023-01-11 12:26:49 +1100

    Try just imageURL

--- index.js
+++ index.js
@@ -202,15 +202,15 @@ console.log(otherOriginalNFT);
 // 		return new Response('Invalid "image" value', { status: 400 })
 // 	}
 	  
-	// Build a request that passes through request headers
-	const imageRequest = new Request(imageURL, {
-		headers: request.headers
-	});
-console.log("imageRequest");	  
-console.log(imageRequest);
+// 	// Build a request that passes through request headers
+// 	const imageRequest = new Request(imageURL, {
+// 		headers: request.headers
+// 	});
+// console.log("imageRequest");	  
+// console.log(imageRequest);
 
 	// Returning fetch() with resizing options will pass through response with the resized image.
-	const resizeResponse = await fetch(imageRequest, options);
+	const resizeResponse = await fetch(imageURL, options);
 console.log("resizeResponse");	  
 console.log(resizeResponse);
 console.log(JSON.stringify(resizeResponse));
